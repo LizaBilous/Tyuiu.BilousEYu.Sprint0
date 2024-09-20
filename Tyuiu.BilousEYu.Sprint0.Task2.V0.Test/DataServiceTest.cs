540ca5967aeae3f505a38b81c82a72d6fa6e0e4a
Bilous.liza@bk.ru <Bilous.liza@bk.ru>
2024-09-20 20:44:29 +0500

    Task2

---
+++ Tyuiu.BilousEYu.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Tyuiu.BilousEYu.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.BilousEYu.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Игорь";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Игорь",res);
+        }
+    }
+}
---
+++ Tyuiu.BilousEYu.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.BilousEYu.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.BilousEYu.Sprint0.Task2.V0;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.GetMessage("Игорь"));
+        Console.ReadKey();
+        Console.ReadKey();
+
+    }
+}
